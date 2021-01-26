797aa103a76327230d694a4f1bdcf0423173a64f
Preston Provins <pprovins@vols.utk.edu>
2021-01-26 16:14:20 -0500

    Hot fix run

--- preprocessing/run.sh
+++ preprocessing/run.sh
@@ -3,8 +3,8 @@
 # This script (in the near future) will include the list of tasks needed from start
 # to finish to run the whole preprocessing steps for Enviromapper
 
-./extract_subcategory.sh layers/cat_type_disturbance.geojson cat_type_disturbance FCSUBTYPE
-./merge_disturbance_subcategory.sh
+#./extract_subcategory.sh layers/cat_type_disturbance.geojson cat_type_disturbance FCSUBTYPE
+#./merge_disturbance_subcategory.sh
 
 #./extract_subcategory.sh layers/cat_soil_drainage_class.geojson cat_soil_drainage_class DrainClass
 # merge?
@@ -12,7 +12,7 @@
 #./extract_subcategory.sh layers/cat_avg_yearly_rainfall.geojson cat_avg_yearly_rainfall RANGE
 # merge?
 
-#./extract_subcategory.sh layers/cat_understory_density_class.geojson cat_understory_density_class density
+./extract_subcategory.sh layers/cat_understory_density_class.geojson cat_understory_density_class density
 # merge?
 
 #./extract_subcategory.sh layers/cat_geology.geojson cat_geology AGE_PERIOD
