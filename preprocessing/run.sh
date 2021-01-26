8d7d7c2243ab5e68343e52f58389d8d9ba6b786e
Preston Provins <pprovins@vols.utk.edu>
2021-01-26 15:47:59 -0500

    Hot fix run

--- preprocessing/run.sh
+++ preprocessing/run.sh
@@ -6,3 +6,13 @@
 ./extract_subcategory.sh layers/cat_type_disturbance.geojson cat_type_disturbance FCSUBTYPE
 ./merge_disturbance_subcategory.sh
 
+./extract_subcategory.sh layers/cat_soil_drainage_class.geojson cat_soil_drainage_class FCSUBTYPE
+# merge?
+
+./extract_subcategory.sh layers/cat_avg_yearly_rainfall.geojson cat_avg_yearly_rainfall FCSUBTYPE
+# merge?
+
+./extract_subcategory.sh layers/cat_understory_density_class.geojson cat_understory_density_class FCSUBTYPE
+# merge?
+
+./extract_subcategory.sh layers/cat_geology.geojson cat_geology FCSUBTYPE
