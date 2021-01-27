d6b2bd5529329d7b56c917c59cdc8fe41826c02f
Preston Provins <pprovins@vols.utk.edu>
2021-01-27 16:40:20 -0500

    Hot fix subcat, pt 3

--- preprocessing/extract_subcategory.sh
+++ preprocessing/extract_subcategory.sh
@@ -31,7 +31,9 @@ cat $1 | grep $variable_name | uniq | sed -E -e "s/^[ ]+\"$variable_name\" : [\"
 while read -r line;
 do
     echo $line
+    line=${line//[!0-9]/}
     line=$(echo $line | dos2unix)
+    echo $line
     gdal_rasterize -ot Byte -a_nodata 0 -ts 3128 1192 -burn 253 -burn 142 -burn 31 -where "$variable_name='$line'" $1 tmp/temp.tif
     line=$(echo $line | sed 's/ /_/g' | sed -E -e 's/[;\,-/]/_/g' | sed 's/__/_/g' | sed 's/[)(]//g' | tr '[:upper:]' '[:lower:]')
     gdal_translate tmp/temp.tif tmp/"$layer"_"$line"_2.tif
