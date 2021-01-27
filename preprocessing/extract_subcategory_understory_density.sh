a660bf68e6163bc13e6ea05d7068735ef6529b9f
Preston Provins <pprovins@vols.utk.edu>
2021-01-27 16:32:29 -0500

    Added new understory extraction

--- preprocessing/extract_subcategory_understory_density.sh
+++ preprocessing/extract_subcategory_understory_density.sh
@@ -13,32 +13,32 @@ layer=$2
 variable_name=Dens_Class
 counter=0
 
-    line="Light"
-    echo $line
-    line=$(echo $line | dos2unix)
-    echo "Doing raster"
-    gdal_rasterize -ot Byte -a_nodata 0 -a OBJECTID -l OGRGeoJSON -ts 3128 1192 -burn 247 -burn 105 -burn 164 -where "$variable_name LIKE '%$line%'" $1 tmp/temp.tif
-    echo "Raster complete"
-    line=$(echo $line | sed 's/ /_/g' | sed -E -e 's/[;\,-/]/_/g' | sed 's/__/_/g' | sed 's/[)(]//g' | tr '[:upper:]' '[:lower:]')
-    gdal_translate tmp/temp.tif tmp/"$layer"_"$line"_1.tif
-    #gdal_translate -a_srs EPSG:4326 tmp/temp.tif tmp/"$layer"_"$line"_1.tif
-    counter=$(( counter+1 ))
+line="Light"
+echo $line
+line=$(echo $line | dos2unix)
+echo "Doing raster"
+gdal_rasterize -ot Byte -a_nodata 0 -ts 3128 1192 -burn 247 -burn 105 -burn 164 -where "$variable_name LIKE '%$line%'" $1 tmp/temp.tif
+echo "Raster complete"
+line=$(echo $line | sed 's/ /_/g' | sed -E -e 's/[;\,-/]/_/g' | sed 's/__/_/g' | sed 's/[)(]//g' | tr '[:upper:]' '[:lower:]')
+gdal_translate tmp/temp.tif tmp/"$layer"_"$line"_1.tif
+#gdal_translate -a_srs EPSG:4326 tmp/temp.tif tmp/"$layer"_"$line"_1.tif
+counter=$(( counter+1 ))
 
-    line="Medium"
-    echo $line
-    line=$(echo $line | dos2unix)
-    gdal_rasterize -ot Byte -a_nodata 0 -a OBJECTID -l OGRGeoJSON -ts 3128 1192 -burn 196 -burn 28 -burn 142 -where "$variable_name LIKE '%$line%'" $1 tmp/temp.tif
-    line=$(echo $line | sed 's/ /_/g' | sed -E -e 's/[;\,-/]/_/g' | sed 's/__/_/g' | sed 's/[)(]//g' | tr '[:upper:]' '[:lower:]')
-    gdal_translate tmp/temp.tif tmp/"$layer"_"$line"_1.tif
-    counter=$(( counter+1 ))
+line="Medium"
+echo $line
+line=$(echo $line | dos2unix)
+gdal_rasterize -ot Byte -a_nodata 0 -ts 3128 1192 -burn 196 -burn 28 -burn 142 -where "$variable_name LIKE '%$line%'" $1 tmp/temp.tif
+line=$(echo $line | sed 's/ /_/g' | sed -E -e 's/[;\,-/]/_/g' | sed 's/__/_/g' | sed 's/[)(]//g' | tr '[:upper:]' '[:lower:]')
+gdal_translate tmp/temp.tif tmp/"$layer"_"$line"_1.tif
+counter=$(( counter+1 ))
 
-    line="Heavy"
-    echo $line
-    line=$(echo $line | dos2unix)
-    gdal_rasterize -ot Byte -a_nodata 0 -a OBJECTID -l OGRGeoJSON -ts 3128 1192 -burn 121 -burn 0 -burn 124 -where "$variable_name LIKE '%$line%'" $1 tmp/temp.tif
-    line=$(echo $line | sed 's/ /_/g' | sed -E -e 's/[;\,-/]/_/g' | sed 's/__/_/g' | sed 's/[)(]//g' | tr '[:upper:]' '[:lower:]')
-    gdal_translate tmp/temp.tif tmp/"$layer"_"$line"_1.tif
-    counter=$(( counter+1 ))
+line="Heavy"
+echo $line
+line=$(echo $line | dos2unix)
+gdal_rasterize -ot Byte -a_nodata 0 -ts 3128 1192 -burn 121 -burn 0 -burn 124 -where "$variable_name LIKE '%$line%'" $1 tmp/temp.tif
+line=$(echo $line | sed 's/ /_/g' | sed -E -e 's/[;\,-/]/_/g' | sed 's/__/_/g' | sed 's/[)(]//g' | tr '[:upper:]' '[:lower:]')
+gdal_translate tmp/temp.tif tmp/"$layer"_"$line"_1.tif
+counter=$(( counter+1 ))
 
 #counter=0
 #echo -e "Heavy\nMedium\nLight" | 
--- preprocessing/merge_understory_density.sh
+++ preprocessing/merge_understory_density.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 counter=0
-    i="tmp/density_light_1.tif"
+    i="tmp/cat_understory_density_class_light_1.tif"
     echo $i;
     name=$(echo "density_"$counter"")
     cp $i sublayers/$name.tif
     counter=$(( counter+1 ))
 
-    i="tmp/density_medium_1.tif"
+    i="tmp/cat_understory_density_class_medium_1.tif"
     echo $i;
     name=$(echo "density_"$counter"")
     cp $i sublayers/$name.tif
     counter=$(( counter+1 ))
 
-    i="tmp/density_heavy_1.tif"
+    i="tmp/cat_understory_density_class_heavy_1.tif"
     echo $i;
     name=$(echo "density_"$counter"")
     cp $i sublayers/$name.tif
