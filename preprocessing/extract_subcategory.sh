cbb1e3a4395c1919ecc475cf6084a540beb6aada
Preston Provins <pprovins@vols.utk.edu>
2021-01-27 16:38:28 -0500

    Hot fix subcat

--- preprocessing/extract_subcategory.sh
+++ preprocessing/extract_subcategory.sh
@@ -19,7 +19,8 @@ do
     line=${line//[!0-9]/}
     line=$(echo $line | dos2unix)
     echo $line
-    gdal_rasterize -ot Byte -a_nodata 0 -ts 3129 1192 -burn 253 -burn 142 -burn 31 -where "$variable_name='$line'" $1 tmp/temp.tif    line=$(echo $line | sed 's/ /_/g' | sed -E -e 's/[;\,-/]/_/g' | sed 's/__/_/g' | sed 's/[)(]//g' | tr '[:upper:]' '[:lower:]')
+    gdal_rasterize -ot Byte -a_nodata 0 -ts 3129 1192 -burn 253 -burn 142 -burn 31 -where "$variable_name='$line'" $1 tmp/temp.tif
+    line=$(echo $line | sed 's/ /_/g' | sed -E -e 's/[;\,-/]/_/g' | sed 's/__/_/g' | sed 's/[)(]//g' | tr '[:upper:]' '[:lower:]')
     gdal_translate tmp/temp.tif tmp/"$layer"_"$line"_1.tif
     #gdal_translate -a_srs EPSG:4326 tmp/temp.tif tmp/"$layer"_"$line"_1.tif
     counter=$(( counter+1 ))
@@ -30,10 +31,9 @@ cat $1 | grep $variable_name | uniq | sed -E -e "s/^[ ]+\"$variable_name\" : [\"
 while read -r line;
 do
     echo $line
-    line=${line//[!0-9]/}
     line=$(echo $line | dos2unix)
-    echo $line
-    gdal_rasterize -ot Byte -a_nodata 0 -ts 3129 1192 -burn 253 -burn 142 -burn 31 -where "$variable_name='$line'" $1 tmp/temp.tifline=$(echo $line | sed 's/ /_/g' | sed -E -e 's/[;\,-/]/_/g' | sed 's/__/_/g' | sed 's/[)(]//g' | tr '[:upper:]' '[:lower:]')
+    gdal_rasterize -ot Byte -a_nodata 0 -a OBJECTID -l OGRGeoJSON -ts 3128 1192 -burn 253 -burn 142 -burn 31 -where "$variable_name='$line'" $1 tmp/temp.tif
+    line=$(echo $line | sed 's/ /_/g' | sed -E -e 's/[;\,-/]/_/g' | sed 's/__/_/g' | sed 's/[)(]//g' | tr '[:upper:]' '[:lower:]')
     gdal_translate tmp/temp.tif tmp/"$layer"_"$line"_2.tif
     counter=$(( counter+1 ))
 done;
